12d3a9bdd1073fa877ed556e91dfcbb3416c5045
VINAY CHAURASIYA <92705023+VinayChaurasiyaA@users.noreply.github.com>
2023-04-03 00:24:10 +0530

    Update connection.js

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 // put your mongodb url! and then enter into admins panel and then create events, and also register!
 mongoose
   .connect(
-    "mongodb+srv://vinay:password12345@cluster0.l6vllqy.mongodb.net/?retryWrites=true&w=majority",
+   // put your databse
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
